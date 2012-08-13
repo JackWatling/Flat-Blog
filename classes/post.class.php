aa3077284a37e877e7f20f46607a1a8670de119e
Jack Watling <jackwatling@gmail.com>
2012-08-13 20:48:49 +0100

    Implemented basic Post class
    This is used to hold all data relevant to a post. When instantiated the
    file is passed to the constructor, which pulls in the data and decodes
    the json, the variables are then assigned to those within the class.
    This will happen for each post instance.

---
+++ classes/post.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class Post{
+
+	private $title;
+	private $date;
+	private $author;
+	private $content;
+
+	public function __construct( $file ){
+		$data = json_decode( file_get_contents( $file ) );
+
+		$this->title = $data->title;
+		$this->date = $data->date;
+		$this->author = $data->author;
+		$this->content = $data->content;
+	}
+
+	public function get_title(){
+		return $this->title;
+	}
+
+	public function get_date(){
+		return $this->date;
+	}
+
+	public function get_author(){
+		return $this->author;
+	}
+
+	public function get_content(){
+		return $this->content;
+	}
+
+}
--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php include 'views/index.tmpl.php'; ?>
+<?php
+
+require_once 'classes/post.class.php';
+
+$posts = glob( 'posts/*.txt' );
+
+foreach ($posts as $key => &$file) {
+	$file = new Post( $file );
+}
+
+include 'views/index.tmpl.php';
+
+?>
