ff63cb8062efe5fca064ce0ec1d81f5c5e77a2f5
Jack Watling <jackwatling@gmail.com>
2012-08-13 20:19:52 +0100

    Separated the index into multiple files
    These include header and footer files, meaning that the other files can
    also use these and it minimises duplicate coding.

---
+++ _partials/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Flat-Blog</title>
+	<link rel="stylesheet" href="fonts/style.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<header class="main wrapper">
+		<h1><a href="index.html">Flat-Blog</a></h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section class="content wrapper">
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php include 'views/index.tmpl.php'; ?>
